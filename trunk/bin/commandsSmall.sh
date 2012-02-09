5ee2ee8aeb567b2adc64a10712a948e098861652
kacper.chwialkowski@gmail.com <kacper.chwialkowski@gmail.com@c4971dc6-cb64-b92d-fb3a-07e0abdd91db>
2012-02-09 13:04:40 +0000

    createDir check working dir and data set

--- trunk/bin/commandsSmall.sh
+++ trunk/bin/commandsSmall.sh
@@ -1,9 +1,28 @@
+#zakladamy ze jestesmy w katalogu glownym
 set -e
 PWD=$(pwd)
-echo $PWD
-PWD=${PWD}/LDAModelCreator
+baseN=$(basename $PWD)
 
-java -jar ${PWD}/target/LDAModelCreator-1.0-jar-with-dependencies.jar \
+echo $baseN
+if [ "$baseN" != "categorizer-lda" ]; then
+	echo "run form categorizer-lda direcotry"
+	exit 1
+fi
+
+echo "wrapper script on mallet and malletmodelcreator"
+
+
+LDACreator=${PWD}/LDAModelCreator
+dataDir=${PWD}/data/$1/
+
+if [ ! -d "$dataDir" ]; then
+	echo "specifide bad dataSet. Dir $1 does not exist"
+	exit 1
+fi
+
+echo "using dataset $dataDir"
+
+java -jar ${LDAModelCreator}/target/LDAModelCreator-1.0-jar-with-dependencies.jar \
 --trainingDir ${PWD}/smallMalletDir \
 --malletFile ${PWD}/models/small/smallData.txt \
 
