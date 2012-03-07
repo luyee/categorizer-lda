6603dfe1e785943821b40c22eddaf548206db9cd
kacper.chwialkowski@gmail.com <kacper.chwialkowski@gmail.com@c4971dc6-cb64-b92d-fb3a-07e0abdd91db>
2012-03-07 00:59:39 +0000

    some bins dependend on my build

--- trunk/bin/prepareForSmallTests.sh
+++ trunk/bin/prepareForSmallTests.sh
@@ -1,17 +1,24 @@
 #bin/bash
 set -e
 PWD=$(pwd)
+dataDir=${PWD}/data/$1
+
+if [ ! -d "$dataDir" ]; then
+        echo "specifide bad dataSet. Dir $1 does not exist"
+        exit 1
+fi
+
+echo "using dataset $dataDir"
+
+
 echo $PWD
-SMALLDIR=${PWD}"/LDAModelCreator/models/small"
 INFDIR=${PWD}"/Inferencer/src/main/resources"
 
-echo $SMALLDIR
 echo $INFDIR
 
-sh ./LDAModelCreator/commandsSmall.sh
-cp ${SMALLDIR}/TrainingMoldel.mallet ${INFDIR}/TrainingMoldel.mallet 
-cp ${SMALLDIR}/Inferencer.mallet     ${INFDIR}/Inferencer.mallet
-cp ${SMALLDIR}/DocPerTopic.txt       ${INFDIR}/DocPerTopic.txt
+cp ${dataDir}/TrainingMoldel.mallet ${INFDIR}/TrainingMoldel.mallet 
+cp ${dataDir}/Inferencer.mallet     ${INFDIR}/Inferencer.mallet
+cp ${dataDir}/DocPerTopic.txt       ${INFDIR}/DocPerTopic.txt
 
 mvn -f ${PWD}/Inferencer/pom.xml clean assembly:single install
 
