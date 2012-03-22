6f1bbf1c31855177dac555d626150017d608d60b
kacper.chwialkowski@gmail.com <kacper.chwialkowski@gmail.com@c4971dc6-cb64-b92d-fb3a-07e0abdd91db>
2012-03-22 18:38:41 +0000

    tarining model inside this module

---
+++ trunk/Inferencer/src/main/java/ldainference/Inferencer.java
@@ -0,0 +1,11 @@
+package ldainference;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kacper
+ * Date: 3/22/12
+ * Time: 7:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Inferencer {
+}
