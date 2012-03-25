965763a4ed2b1472b064e4f4d8582ba662953af9
kacper.chwialkowski@gmail.com <kacper.chwialkowski@gmail.com@c4971dc6-cb64-b92d-fb3a-07e0abdd91db>
2012-03-25 19:07:41 +0000

    working interfeca with distributions

--- Inferencer/src/main/java/svminferencer/SVMModelTrainer.java
+++ Inferencer/src/main/java/svminferencer/SVMModelTrainer.java
@@ -1,12 +1,19 @@
 package svminferencer;
 
+import cc.mallet.pipe.*;
+import cc.mallet.pipe.iterator.CsvIterator;
 import cc.mallet.types.InstanceList;
+import interfaces.AbstractInferencer;
+import interfaces.AbstractModelTrainer;
+import weka.classifiers.Classifier;
 import weka.classifiers.functions.SMO;
 import weka.core.Instances;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,48 +22,129 @@ import java.util.UUID;
  * Time: 7:39 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SVMModelTrainer {
-    private File arrfTrainingFile;
-    private final InstanceListToArff instanceListToArff;
-    private final InstanceList trainingInstanceList;
-    private final SMO smo;
-    private  Set<String> categoriesNames;
+public class SVMModelTrainer extends AbstractModelTrainer{
 
-    public SVMModelTrainer(InstanceList instanceList){
-        this.trainingInstanceList = instanceList;
+    public static final String SVMMODEL_WEKA = "/SVMModel.weka";
+    protected File arrfTrainingFile;
+    protected final InstanceListToArff instanceListToArff;
+    protected  InstanceList trainingInstances;
+    protected Classifier classifier;
+    protected  Set<String> categoriesNames;
+    protected final int numCategories;
+    protected String trainingCsvPath;
+
+    public SVMModelTrainer(String trainingCsvPath, int numCategories){
+        this.modelFromFile=false;
+        this.trainingCsvPath = trainingCsvPath;
+        this.numCategories = numCategories;
         instanceListToArff = new InstanceListToArff();
-        smo = new SMO();
+        classifier = null;
     }
 
-    public SMO getSmo() {
-        return smo;
-    }
 
-    public Set<String> getCategoriesNames() {
-        return categoriesNames;
-    }
+
 
     public void  createArffTrainingFile() throws IOException {
         arrfTrainingFile = File.createTempFile(UUID.randomUUID().toString(), ".arff");
-        //arrfTrainingFile.deleteOnExit();
-        System.out.println("arf file : " + arrfTrainingFile.getAbsolutePath());
+        arrfTrainingFile.deleteOnExit();
+        //System.out.println("arf file : " + arrfTrainingFile.getAbsolutePath());
         PrintWriter pw = new PrintWriter(arrfTrainingFile);
-        instanceListToArff.convert2ARFF(trainingInstanceList, pw, "trainingFile");
+        instanceListToArff.convert2ARFF(trainingInstances, pw, "trainingFile");
         categoriesNames = instanceListToArff.getCategoriesNames();
         pw.flush();
         pw.close();
     }
 
-    public void train() throws Exception {
+
+    @Override
+    public void trainModel() throws Exception {
+        Instances data = loadData();
+
+        if (!modelFromFile){
+            System.out.println("building");
+            createClassifier(data);
+        }
+
+
+    }
+
+    protected void createClassifier(Instances data) throws Exception {
+        classifier = new SMO();
+        classifier.buildClassifier(data);
+    }
+
+    protected Instances loadData() throws IOException {
+        //String trainingCsvPath ="/home/kacper/IdeaProjects/LDAModelCreator/src/test/testsDir/UTFtest/engfile";
+        // Begin by importing documents from text to feature sequences
+        ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
+
+        // Pipes: lowercase, tokenize, remove stopwords, map to features
+        pipeList.add(new CharSequenceLowercase());
+
+        pipeList.add(new CharSequence2TokenSequence(Pattern.compile("[\\p{L}\\p{M}]+")));
+        //pipeList.add( new TokenSequenceRemoveStopwords(new File("stoplists/en.txt"), "UTF-8", false, false, false) );
+        pipeList.add(new TokenSequence2FeatureSequence());
+
+        trainingInstances = new InstanceList(new SerialPipes(pipeList));
+
+        Reader fileReader = new InputStreamReader(new FileInputStream(new File(trainingCsvPath)), "UTF-8");
+        trainingInstances.addThruPipe(new CsvIterator(fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$"),
+                3, 2, 1)); // data, label, name fields
+
+
         createArffTrainingFile();
         BufferedReader reader = new BufferedReader(
                 new FileReader(arrfTrainingFile));
         Instances data = new Instances(reader);
         reader.close();
         data.setClassIndex(data.numAttributes() - 1);
+        return data;
+    }
 
-        smo.buildClassifier(data);
+    @Override
+    public AbstractInferencer getInferencer() {
+        return new SvmInferencer(trainingInstances,
+                classifier,
+                categoriesNames,
+                numCategories);
+    }
 
+    @Override
+    public boolean readModelFromFile(String modelPath) throws Exception{
+        String pathname = modelPath + SVMMODEL_WEKA;
+        try {
 
+            ObjectInputStream ois = new ObjectInputStream(
+                    new FileInputStream(pathname));
+            classifier = castToProprerClassifier(ois);
+            ois.close();
+            this.modelFromFile=true;
+            return true;
+        } catch (Exception e) {
+            System.err.println("Unable to restore saved svn model " +
+                    pathname     + ": " + e);
+            return false;
+        }
+    }
+
+    protected SMO castToProprerClassifier(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+        return (SMO) ois.readObject();
+    }
+
+    @Override
+    public void saveModel(String modelPath) {
+        String pathname = modelPath + SVMMODEL_WEKA;
+        assert (classifier != null);
+        try {
+
+            ObjectOutputStream oos =
+                    new ObjectOutputStream (new FileOutputStream (pathname));
+            oos.writeObject (classifier);
+            oos.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException ("Couldn't write svm model to filename "+pathname);
+        }
     }
 }
--- Inferencer/src/main/java/svminferencer/SvmInferencer.java
+++ Inferencer/src/main/java/svminferencer/SvmInferencer.java
@@ -1,14 +1,13 @@
 package svminferencer;
 
 import cc.mallet.types.InstanceList;
+import interfaces.AbstractInferencer;
 import util.MalletWriter;
 import weka.classifiers.Classifier;
 import weka.core.Instances;
 
 import java.io.*;
-import java.util.Set;
-import java.util.UUID;
-import java.util.Vector;
+import java.util.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,7 +16,7 @@ import java.util.Vector;
  * Time: 9:10 PM
  * To change this template use File | Settings | File Templates.
  */
-public class SvmInferencer {
+public class SvmInferencer extends AbstractInferencer {
 
     
     private final InstanceListToArff instanceListToArff;
@@ -34,7 +33,7 @@ public class SvmInferencer {
         this.trainingInstanceList = trainingInstanceList;
         this.classifier = classifier;
         this.numCategories = numCategories;
-        instanceListToArff = new InstanceListToArff(categoiresNames);
+        instanceListToArff = new InstanceListToArff(categoiresNames,trainingInstanceList);
     }
     
                 /*
@@ -43,7 +42,7 @@ public class SvmInferencer {
 
     public void  createArffToBeClassifedFile() throws IOException {
         arrfToBeClassifedFile = File.createTempFile(UUID.randomUUID().toString(), ".arff");
-//        arrfToBeClassifedFile.deleteOnExit();
+        arrfToBeClassifedFile.deleteOnExit();
         //System.out.println("file to be inf :" + arrfToBeClassifedFile.getAbsolutePath());
         PrintWriter pw = new PrintWriter(arrfToBeClassifedFile);
         instanceListToArff.convert2ARFF(toBeClassifiedInstanceList, pw, "arrfToBeClassifedFile");
@@ -53,8 +52,11 @@ public class SvmInferencer {
         /*
         Create Mallet instance List to be clasified
          */
+        //System.out.println("1: "+trainingInstanceList.getDataAlphabet().size());
         toBeClassifiedInstanceList = MalletWriter.createInsatnceList(instance,trainingInstanceList);
+        //System.out.println("2: "+trainingInstanceList.getDataAlphabet().size());
         createArffToBeClassifedFile();
+        //System.out.println("3: "+trainingInstanceList.getDataAlphabet().size());
         // load unlabeled data
         Instances unlabeled = new Instances(
                 new BufferedReader(
@@ -70,12 +72,30 @@ public class SvmInferencer {
         // label instances
 
         Vector<String> ret = new Vector<String>();
+        double [] distribution;
+        Map<Double,Integer> map = new TreeMap<Double,Integer>();
         for (int i = 0; i < unlabeled.numInstances(); i++) {
+
             double clsLabel = classifier.classifyInstance(unlabeled.instance(i));
+
+            distribution = classifier.distributionForInstance(unlabeled.instance(i));
             labeled.instance(i).setClassValue(clsLabel);
+
+            for( int j=0;j<distribution.length;j++ ){
+
+                map.put(distribution[j],j);
+            }
+
+
+            for(Double d: map.keySet()){
+
+                ret.add(unlabeled.classAttribute().value((int) map.get(d))) ;
+            }
 
             while (ret.size() < numCategories)
                 ret.add(labeled.instance(i).toString(numAtrs));
+
+
         }
         
         return ret;
